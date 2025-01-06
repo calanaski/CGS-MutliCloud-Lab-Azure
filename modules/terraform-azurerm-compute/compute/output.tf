84a21c3102b8f038dca45a3c1279c263479625ca
Lanaski <christina.lanaski@capgemini-gs.com>
2025-01-06 16:52:28 -0500

    testing terraform module

--- modules/terraform-azurerm-compute/compute/output.tf
+++
@@ -1,9 +0,0 @@
-// Create an output of the Resource Group Name
-output "tenant-rg-net-name" {
-  value = azurerm_resource_group.tenant-rg-net.name
-}
-
-// Create an output of the Resource Group Name
-output "tenant-rg-core-name" {
-  value = azurerm_resource_group.tenant-rg-core.name
-}
